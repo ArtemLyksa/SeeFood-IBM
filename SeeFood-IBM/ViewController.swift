3c22a19a6269a73374063f09674de38a952fb76f
Artem <artemlyksa@gmail.com>
2019-09-12 12:35:15 +0300

    Initial commit

---
+++ SeeFood-IBM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood-IBM
+//
+//  Created by Artem Lyksa on 9/12/19.
+//  Copyright © 2019 lyksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
